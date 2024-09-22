17623b3b82f5219231046b6161f7d8fc1828a550
rougeeee <preciano.maglasang@gmail.com>
2024-09-22 20:11:59 +0800

    constraints #1

--- web/constroller.py
+++ web/constroller.py
@@ -1,15 +1,31 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 def students():
+    if request.method == 'POST':
+        firstName = request.form.get('firstName')
+        lastName = request.form.get('lastName')
+        yearLevel = request.form.get('year')
+        gender = request.form.get('gender')
+        course = request.form.get('program')
+
+        if len(firstName) == 0:
+            flash('Invalid first name.', category='error')
+        elif len(lastName) == 0:
+            flash('Invalid last name.', category='error')
+        elif len(yearLevel) < 8:
+            flash('Invalid Year Level.', category='error')
+        else:
+            flash('Student added.', category='success')
+    
     return render_template('students.html')
 
-@views.route('/college/')
+@views.route('/college/', methods=['GET', 'POST'])
 def school():
     return render_template('college.html')
 
-@views.route('/programs')
+@views.route('/programs', methods=['GET', 'POST'])
 def course():
     return render_template('programs.html')
